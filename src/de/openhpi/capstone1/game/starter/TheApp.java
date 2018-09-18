967960cf1c81a7deb1c004eea09add9dc54cbcbf
BpeDuHa <lyubov.kondyushova@gmail.com>
2018-09-18 21:21:40 +0200

    Added lila ball which appears on mouse click

--- src/de/openhpi/capstone1/game/starter/TheApp.java
+++ src/de/openhpi/capstone1/game/starter/TheApp.java
@@ -1,17 +1,25 @@
 package de.openhpi.capstone1.game.starter;
 
 
+
+import de.openhpi.capstone1.game.view.AbstractView;
+import de.openhpi.capstone1.game.view.BallView;
 import processing.core.PApplet;
 
 public class TheApp extends PApplet {
-
+	
+	private AbstractView ballView;
+	
+	
 	@Override
 	public void settings() {
-		
+		size(500,500);
 	}
 
 	@Override
 	public void setup() {  // setup() runs once
+		frameRate(30);
+		ballView = new BallView(this);
 		
 	}
 
@@ -23,6 +31,6 @@ public class TheApp extends PApplet {
 	//Add further user interaction as necessary
 	@Override
 	public void mouseClicked() {
-
+		ballView.update();
 	}
 }
---
+++ src/de/openhpi/capstone1/game/view/BallView.java
@@ -0,0 +1,19 @@
+package de.openhpi.capstone1.game.view;
+
+import processing.core.PApplet;
+
+public class BallView extends AbstractView{
+	//protected PApplet display;
+	
+	public BallView(PApplet display) {
+		super(display);
+	}
+	
+@Override	
+	public void update() {
+		display.background(204);
+		display.fill(153,0,153); //lila color
+		display.ellipse(120, 120, 50, 50); // first two values define the position, second two values - the size
+		display.redraw();
+	};
+}
