7ccb36cfc777377cfe7b8fffb6fb24e74b91c0d9
Patrick Thomas <patrick-thomas@gmx.net>
2018-09-21 20:11:47 +0200

    Cleaned up style. Use CTRL+Shift+F for auto-format on java files

--- src/de/openhpi/capstone1/game/view/BallView.java
+++ src/de/openhpi/capstone1/game/view/BallView.java
@@ -4,14 +4,14 @@ import processing.core.PApplet;
 
 public class BallView extends AbstractView {
 	// protected PApplet display;
-    int rad = 25;        // radius of the ball
-    float xpos=200, ypos=119;    // Starting position of shape    
+	int rad = 25; // radius of the ball
+	float xpos = 200, ypos = 119; // Starting position of shape
 
-    float xspeed = 6;  // Speed of the shape
-    float yspeed = 6;  // Speed of the shape
+	float xspeed = 6; // Speed of the shape
+	float yspeed = 6; // Speed of the shape
 
-    int xdirection = -1;  // Left to Right
-    int ydirection = 1;  // Top to Bottom
+	int xdirection = -1; // Left to Right
+	int ydirection = 1; // Top to Bottom
 
 	public BallView(PApplet display) {
 		super(display);
@@ -20,23 +20,21 @@ public class BallView extends AbstractView {
 	@Override
 	public void update() {
 		display.ellipseMode(display.RADIUS);
-		//display.background(204);
 		display.fill(153, 0, 153); // lila color
-//		
-//		display.ellipse(120, 120, 50, 50); // first two values define the position, second two values - the size
-//		display.redraw();
-		xpos = xpos + ( xspeed * xdirection );
-	      ypos = ypos + ( yspeed * ydirection );
-	      if (xpos > display.width-rad || xpos < rad) { //if the ball reaches left or right screen side, then bounce
-	          xdirection *= -1;
-	        }
-	        if (ypos < rad) {//if the ball reaches top of the screen, then bounce
-	          ydirection *= -1;
-	        }
-//Comment next 3 lines, so that the ball can dissappear through the bottom	        
-	        if (ypos > display.height-rad) {//if the ball reaches the bottom of the screen, then bounce
-		          ydirection *= -1;
-		        }
-	        display.ellipse(xpos, ypos, rad, rad);
+	
+		xpos = xpos + (xspeed * xdirection);
+		ypos = ypos + (yspeed * ydirection);
+		if (xpos > display.width - rad || xpos < rad) { // if the ball reaches left or right screen side, then bounce
+			xdirection *= -1;
+		}
+		if (ypos < rad) {// if the ball reaches top of the screen, then bounce
+			ydirection *= -1;
+		}
+		
+		//Comment next 3 lines, so that the ball can dissappear through the bottom	        
+		if (ypos > display.height - rad) {// if the ball reaches the bottom of the screen, then bounce
+			ydirection *= -1;
+		}
+		display.ellipse(xpos, ypos, rad, rad);
 	};
 }
--- src/de/openhpi/capstone1/game/view/PaddleView.java
+++ src/de/openhpi/capstone1/game/view/PaddleView.java
@@ -5,33 +5,28 @@ import processing.core.PApplet;
 public class PaddleView extends AbstractView {
 	// protected PApplet display;
 	int dragX;
-	  
-	  int paddleX=190;
-	  int paddleY=580;
-	  int paddleWidth=100;
-	  int paddleHeight=25;
+
+	int paddleX = 190;
+	int paddleY = 580;
+	int paddleWidth = 100;
+	int paddleHeight = 25;
 
 	public PaddleView(PApplet display) {
-		super(display); 
+		super(display);
 	}
 
-
-	
 	@Override
 	public void update() {
 		display.fill(255, 255, 255); // white color
-		dragX=display.mouseX; //read current mouse x-axis position
-		
-        if (dragX<=0) { //if the mouse goes beyond the left side of the canvas
-     dragX=0; //then set the paddle to the left-most edge of the canvas
-   }else if (dragX>=display.width-paddleWidth) { //if the mouse goes beyond the right side of the canvas/   
-   dragX=display.width-paddleWidth; //then set the paddle to the right-most edge of the canvas
-   }
-        paddleX=dragX;
-		display.rect(paddleX, paddleY, paddleWidth, paddleHeight); //x-axis, y-axis, length, height
+		dragX = display.mouseX; // read current mouse x-axis position
+
+		if (dragX <= 0) { // if the mouse goes beyond the left side of the canvas
+			dragX = 0; // then set the paddle to the left-most edge of the canvas
+		} else if (dragX >= display.width - paddleWidth) { // if the mouse goes beyond the right side of the canvas/
+			dragX = display.width - paddleWidth; // then set the paddle to the right-most edge of the canvas
 		}
-
-
-	
+		paddleX = dragX;
+		display.rect(paddleX, paddleY, paddleWidth, paddleHeight); // x-axis, y-axis, length, height
+	}
 
 }
